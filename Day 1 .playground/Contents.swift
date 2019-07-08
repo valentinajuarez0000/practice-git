e1efff749a2788437b2116f3ea795554e3a25af7
Alice Zhang <alicezhang32092@gmail.com>
2019-07-08 14:56:42 -0700

    first save

---
+++ Day 1 .playground/Contents.swift
@@ -0,0 +1,65 @@
+import Cocoa
+
+var str = "Hello, playground"
+var age = 15
+"I am \(age) years old"
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+var anotherWelcome = string1 + string2
+print("hello world")
+var first = "karlie"
+var last = "kloss"
+"\(first)\(last)"
+"\(last)\(first)"
+"\(first)\( last)"
+"i love \(first) \(last)"
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+Double(a+b+c+d)/4
+var name = "Valentina"
+var address = "11065 Ferina St"
+let birthday = "November 4, 2003"
+var grade = "Junior"
+var Age = "fifteen"
+let eyecolor = "brown"
+let favoritecolor = "pink"
+Double(10.25*20)
+var Kyara = "Bazzi"
+var StringtoCompress = "Angeles"
+print("\(StringtoCompress.first!)\(StringtoCompress.count-2)\(StringtoCompress.last!)")
+5 < 3
+12 > 7
+6 != 8
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+var luckyNum = 7
+var dogAge = 2
+if dogAge < 2 {
+    print("You are a puppy 🐶")
+}
+if dogAge == 2{
+    print ("You are awesome!")
+}
+if dogAge == 3 {
+    print ("Your are awesome!")
+}
+var  ff = "McDonolds"
+if ff == "McDonolds"{
+    print("estoy feliz")
+}
+var hasAccount = true
+if hasAccount == false {
+    print ("Login")
+    
+}
+if hasAccount != false {
+    print ("create an account")
+}
+var gradeLevel = 7
+if grade <= 6 {print ("elementry school")
+}
+
